0b72623fd25333e31111583a878e9c900a602fd2
Freddy Ayala <freddy.ayala@unmsm.edu.pe>
2024-06-20 21:39:03 -0500

    Contiene el código para SubirExamen, también una carpeta Main con el archivo manage.py

--- Desarrollo/EvaEduca/codificación/Main/settings.py
+++ Desarrollo/EvaEduca/codificación/Main/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "SubirExamen",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = "codificación.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR, 'SubirExamen', 'Templates'), # Agrega la ruta a la carpeta Templates,
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
