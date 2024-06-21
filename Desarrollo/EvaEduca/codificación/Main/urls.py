9477b9b4521928fbed0faf137407769d3f9a98bf
Diego Guzmán <diego.guzman@unmsm.edu.pe>
2024-06-20 23:26:28 -0500

    Agregando apps y urls

--- Desarrollo/EvaEduca/codificación/Main/urls.py
+++ Desarrollo/EvaEduca/codificación/Main/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('subirexamen/', include('SubirExamen.urls')),
+    path('subir-rubrica/', include('SubirRubrica.urls')),
     path('', views.index, name='index'),  # Ejemplo de una vista llamada 'index'
 ]
