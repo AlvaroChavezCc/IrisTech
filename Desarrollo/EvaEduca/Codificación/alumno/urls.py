2d84b92850c8436b132beeaecf9eb190dda332ee
Freddy Ayala <freddy.ayala@unmsm.edu.pe>
2024-06-30 13:35:40 -0500

    contiene las urls de login, logout, bienvenida, lista_tareas y subir_respuesta

--- Desarrollo/EvaEduca/Codificación/alumno/urls.py
+++ Desarrollo/EvaEduca/Codificación/alumno/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from alumno import views
 
 urlpatterns = [
-    path('', views.inicio, name="alumno"),
-    
+      
     path('login/', views.login_alumno, name='login_alumno'),
     path('logout/', views.logout_alumno, name='logout_alumno'),
-    path('descargar-examen/', views.descargar_examen, name='descargar_examen'),
-    path('cargar-respuesta/<int:tareas_id>/', views.cargar_respuesta, name='cargar_respuesta'),
+    path('bienvenida/', views.bienvenida, name='bienvenida_alumno'),
+    path('subir_respuesta/', views.subir_respuesta, name='subir_respuesta'),
+    path('tareas/', views.lista_tareas, name='lista_tareas'),
 ]
