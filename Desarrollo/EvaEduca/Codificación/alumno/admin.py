420dad860052ac45539e062df3b563beaace9288
Freddy Ayala <freddy.ayala@unmsm.edu.pe>
2024-06-30 13:29:43 -0500

    contiene los  administradores de modelos alumno y evaluacion

--- Desarrollo/EvaEduca/Codificación/alumno/admin.py
+++ Desarrollo/EvaEduca/Codificación/alumno/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import alumno, Examen, Respuesta
+from .models import alumno, evaluacion
 
 # Register your models here.
 admin.site.register(alumno)
-admin.site.register(Examen)
-admin.site.register(Respuesta)
+admin.site.register(evaluacion)
 
