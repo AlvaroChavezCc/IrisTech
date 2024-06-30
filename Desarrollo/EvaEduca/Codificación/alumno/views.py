09cd3995d681f93c9da5655192a3669a34ef2013
Freddy Ayala <freddy.ayala@unmsm.edu.pe>
2024-06-30 13:43:09 -0500

    contiene las vistas de login, logout, bienvenida, lista_tareas y subir_respuesta. Al ejecutar hay que rutear alumno/login, luego te redirige a la vista de bienvenida donde eliges si quieres descargar tareas (te redirije a una vista donde el docente ha subido las tareas por curso donde puedes descargarlas), la otra opción es que quieras subir las tareas ya resueltas (Hay el inconveniente que te pide autentificación, hay que otorgarle permisos al usuario)

--- Desarrollo/EvaEduca/Codificación/alumno/views.py
+++ Desarrollo/EvaEduca/Codificación/alumno/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import tareas
+from administrador.models import tareas
 from django.contrib.auth import authenticate, login, logout
-from .forms import SubirRespuestaForm, LoginForm
-from .models import Examen, alumno
+from .forms import LoginForm, EvaluacionForm
+from .models import alumno
 
 
 def login_alumno(request):
@@ -17,7 +17,7 @@ def login_alumno(request):
                 user = alumno.objects.get(usuario=usuario)
                 if contrasena == user.contrasena:
                     request.session['alumno_id'] = user.id
-                    return redirect('cargar-respuesta')
+                    return redirect('bienvenida_alumno')
                 else:
                     messages.error(request, 'Contraseña incorrecta')
             except alumno.DoesNotExist:
@@ -30,63 +30,44 @@ def login_alumno(request):
 
 def logout_alumno(request):
     request.session.flush()  # Limpiar la sesión
-    return redirect('alumno_login')
+    return redirect('login_alumno')
 
 
-#@login_required
-def descargar_examen(request):
-    examen_id = request.GET.get('examen_id')
-    examen = get_object_or_404(Examen, pk=examen_id)
-    
-    # Suponiendo que `archivo` es el campo que contiene el archivo que deseas descargar
-    if examen.archivo:
-        file_path = examen.archivo.path  # Obtén la ruta del archivo en el sistema de archivos
-        with open(file_path, 'rb') as f:
-            response = HttpResponse(f.read(), content_type='application/octet-stream')
-            response['Content-Disposition'] = 'attachment; filename=' + examen.archivo.name
-            return response
-    else:
-        # Manejo si el examen no tiene archivo adjunto
-        return HttpResponse('El examen seleccionado no tiene archivo adjunto.')
+def bienvenida(request):
+    mensaje_bienvenida = "Bienvenido!"
+
+    return render(request, 'alumno/Bienvenida.html', {'mensaje_bienvenida': mensaje_bienvenida})
+
+def lista_tareas(request):
+    tareas_alumno = tareas.objects.all()  
+    print(tareas_alumno) 
+    return render(request, 'alumno/Lista_tareas.html', {'tareas': tareas_alumno})
 
 
-#@login_required
-def cargar_respuesta(request, tareas_id):
-    # Obtener la tarea según el ID proporcionado en la URL
-    tarea = get_object_or_404(tareas, id=tareas_id)
-    
+
+def subir_respuesta(request):
     if request.method == 'POST':
-        form = SubirRespuestaForm(request.POST, request.FILES)
+        form = EvaluacionForm(request.POST, request.FILES)
         if form.is_valid():
-            respuesta = form.save(commit=False)
-            respuesta.alumno = request.user  # Asignar el usuario actual como alumno
-            respuesta.tareas = tarea  # Asignar la tarea seleccionada
-            respuesta.save()
-            messages.success(request, 'Respuesta cargada exitosamente.')
-            return redirect('cargar_respuesta', tareas_id=tareas_id) 
+            # Obtener el ID del alumno actualmente autenticado
+            id_alumno_id = request.user.id  # Asumiendo que el usuario está autenticado
+            
+            # Guardar la evaluación en la base de datos asignando id_alumno_id
+            evaluacion_obj = form.save(commit=False)
+            evaluacion_obj.id_alumno_id = id_alumno_id
+            evaluacion_obj.save()
+
+            return redirect('bienvenida_alumno')
     else:
-        form = SubirRespuestaForm()
-    
-    return render(request, 'alumno/cargar_respuesta.html', {'form': form, 'tarea': tarea})
+        form = EvaluacionForm()
+
+    return render(request, 'alumno/Subir_respuesta.html', {'form': form})
 
 
 
-#@login_required  # Asegura que solo usuarios autenticados puedan acceder a esta vista
-def inicio(request):
-    if 'alumno_id' in request.session:
-        alumno_id = request.session['alumno_id']
-        try:
-            profesor_actual = alumno.objects.get(id=alumno_id)
-            name = profesor_actual.nombre
-        except alumno.DoesNotExist:
-            print("Alumno no encontrado.")
-            return redirect('login')
-    else:
-        print("Alumno ID no encontrado en sesión.")
-        return redirect('login')
 
-    return render(request, 'alumno/login.html', {
-        'name': name
-    })
+
+
+
 
   
