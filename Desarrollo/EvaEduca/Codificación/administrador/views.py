11aaf4c2d4806f83d0216842b5ff7659293db639
Alvaro Chavez <135331266+AlvaroChavezCc@users.noreply.github.com>
2024-06-30 23:05:36 -0500

    Main, últimos ajustes

--- Desarrollo/EvaEduca/Codificación/administrador/views.py
+++ Desarrollo/EvaEduca/Codificación/administrador/views.py
@@ -34,7 +34,7 @@ def login_view(request):
     return render(request, 'login.html', {'form': form})
 
 def logout_view(request):
-    logout(request)
+    request.session.flush()
     return redirect('login')
 
 # Vistas de Profesor
